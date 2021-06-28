cb0f92781e4c22fe642f81920ba6336ea4746d10
Grzegorz Stefański <stefanski.grzegorz6@gmail.com>
2021-06-29 01:41:18 +0200

    v0.0.2

--- env.sh
+++ env.sh
@@ -2,7 +2,7 @@ export AZ_RESOURCE_GROUP=io
 export AZ_DATABASE_NAME=XXXXXX-tmp-spring-jpa-mysql
 export AZ_LOCATION=westeurope
 export AZ_MYSQL_USERNAME=spring
-export AZ_MYSQL_PASSWORD=XXXXXXXXXXXXXXXXXXX
+export AZ_MYSQL_PASSWORD=ioPass_666_lol
 export AZ_LOCAL_IP_ADDRESS=$(curl http://whatismyip.akamai.com/)
 export SPRING_DATASOURCE_URL=jdbc:mysql://$AZ_DATABASE_NAME.mysql.database.azure.com:3306/demo?serverTimezone=UTC
 export SPRING_DATASOURCE_USERNAME=spring@$AZ_DATABASE_NAME
